6bc71c3318772c0d03c73a8cff7b91b590999fad
Evgeny <isimvol@gmail.com>
2015-08-30 17:53:41 -0700

    moved backend to a different server

---
+++ FrontEnd/scripts/authentication.js
@@ -0,0 +1,88 @@
+/*
+	Authors: 
+    - Evgeny Makarov 
+    - Kimar Arakaki Neves
+    CPSC 2261 - Web Technology (Term Project/Summer 2015)
+    Last modified: 05 AUG 2015
+*/
+'use strict';
+
+// angular.module('myApp')
+angular.module('myApp').factory('Authentication', function Authentication($http, $rootScope, $location, MySession){
+
+	var loggedStatus = false;
+	$rootScope.DURL = 'http://52.24.4.41:8080';
+	return {
+
+		//login method
+		login: function(user){
+
+			var $promise = $http.post($rootScope.DURL + "/user/login",user);
+
+			$promise.then(function(response){
+				if (response.data["success"]== "ok") {
+
+					MySession.set('email', user.email);
+					MySession.set('token', response.data["token"]);
+					loggedStatus = true;		
+					$location.path('/user');
+
+				} else {
+					console.log('Invalid credentials.');
+					$location.path('/login');
+				}
+			});
+
+		}, 
+		logout: function(){
+			var session ={
+				"token" : MySession.get('token')
+			};
+			var $promise = $http.post($rootScope.DURL + "/user/logout",session);
+
+			$promise.then(function(response){
+				if (response.data["success"]== "ok") {
+					MySession.destroy('email');
+					MySession.destroy('token');
+					$location.path('/login');
+				}
+			});
+			
+		}, 
+		isLogged: function(){
+
+			var session ={
+				"email" : MySession.get('email'),
+				"token" : MySession.get('token')
+			};
+
+			var $promise = $http.post($rootScope.DURL + "/user/session/validate",session);
+
+			$promise.then(function(response){
+				if (response.data["success"]== "ok") {
+					loggedStatus = true;
+				} else {
+					loggedStatus = false;
+				}
+			});
+
+			return loggedStatus;
+		}
+	}
+});
+
+angular.module('myApp').factory('MySession', [ '$http','$rootScope', function Authentication($http,$rootScope){
+	var authenticatedUser = null;
+
+	return {
+		set: function(key, value){
+			return sessionStorage.setItem(key,value);
+		},
+		get: function(key){
+			return sessionStorage.getItem(key);
+		},
+		destroy: function(key){
+			return sessionStorage.removeItem(key);
+		}
+	}
+}]);
---
+++ FrontEnd/scripts/groupControllers.js
@@ -0,0 +1,326 @@
+/*
+  Authors: 
+    - Evgeny Makarov 
+    - Kimar Arakaki Neves
+    CPSC 2261 - Web Technology (Term Project/Summer 2015)
+    Last modified: 05 AUG 2015
+*/
+
+// Controller for /group page. Gets users information
+myApp.controller('groupsController',function($scope,$rootScope,$http){
+  var URL = 'http://52.24.4.41:8080';
+  $scope.myArray = [];
+
+  $scope.init = function() {
+
+    $http({
+      url: URL + '/groups',
+      method: 'GET',
+      dataType: 'json',
+      data: '',
+    }).success(function (data, status, headers, config) {
+
+      // If data exists then display variables
+      if (data) {
+
+        $scope.groups = data;
+
+      }
+
+    }).error(function (data){
+
+        // In case of an error display the message in console
+        console.log("error: " + data);
+
+    });
+  }
+
+  $scope.initCreateGroups = function() {
+    $scope.newgroup = {
+      members: [],
+      category: [],
+      meeting_time: []
+    };
+
+  }
+  
+  $scope.initEditGroups = function() {
+  console.log($rootScope.selectedGroup );
+    
+    $http({
+      url: URL + '/group/'+parseInt($rootScope.selectedGroup),
+      method: 'GET',
+      dataType: 'json'
+    }).success(function (data, status, headers, config) {
+
+      // If data exists then display variables
+      if (data) {
+
+        $scope.newgroup = data[0];
+
+      }
+
+    }).error(function (data){
+
+        // In case of an error display the message in console
+        console.log("error: " + data);
+
+    });
+
+  }
+
+  $scope.createGroup = function(){
+    console.log($scope.newgroup);
+
+    $scope.newgroup.created = new Date();
+    $scope.newgroup.owner = sessionStorage.getItem("email");
+	$scope.newgroup.members = [ sessionStorage.getItem("email") ];
+
+    $http({
+      url: URL + '/create/groups',
+      method: 'PUT',
+      dataType: 'json',
+      data: $scope.newgroup,
+    }).success(function (data, status, headers, config) {
+
+      console.log(data);
+
+    }).error(function (data){
+
+        // In case of an error display the message in console
+        console.log("error: " + data);
+
+    });
+
+  }  
+
+  $scope.editGroup = function(){
+    console.log($scope.newgroup);
+
+    $http({
+      url: URL + '/update/group/'+$scope.newgroup.group_id,
+      method: 'POST',
+      dataType: 'json',
+      data: $scope.newgroup,
+    }).success(function (data, status, headers, config) {
+
+      console.log(data);
+
+    }).error(function (data){
+
+        // In case of an error display the message in console
+        console.log("error: " + data);
+
+    });
+
+  }
+  
+  $scope.addMember = function (){
+      if( $scope.newgroup.members.indexOf(this.member) <0)
+        $scope.newgroup.members.push(this.member);
+      else
+        alert('Cannot entry a duplicated value');
+  }
+
+  $scope.removeMember = function (){
+    // console.log("Removed "+this.$index);
+    $scope.newgroup.members.splice(this.$index,1); 
+  }
+
+  $scope.addCategory = function (){
+      if( $scope.newgroup.category.indexOf(this.newCategory) <0)
+        $scope.newgroup.category.push(this.newCategory);
+      else
+        alert('Cannot entry a duplicated value');
+  }
+
+  $scope.removeCategory = function (){
+    // console.log("Removed "+this.$index);
+    $scope.newgroup.category.splice(this.$index,1); 
+  }
+
+  $scope.addMeeting = function (){
+      if( $scope.newgroup.meeting_time.indexOf(this.newMeeting) <0)
+        $scope.newgroup.meeting_time.push(JSON.parse(JSON.stringify(this.newMeeting)));
+      else
+        alert('This meeting time has already been added.');
+  }
+
+  $scope.removeMeeting = function (){
+    // console.log("Removed "+this.$index);
+    $scope.newgroup.meeting_time.splice(this.$index,1); 
+  }
+
+  $scope.uploadImage = function(){
+    $scope.newgroup.image = $scope.newgroup.image;
+  }
+
+  $scope.setGroupId = function(){
+    $rootScope.selectedGroup = this.group.group_id;
+    // group_id = this.group.group_id;
+    // console.log($stateParams.group_id);
+  }
+
+  $scope.loadGroupDetails = function(){
+    console.log($rootScope.selectedGroup );
+
+    
+    $http({
+      url: URL + '/group/'+parseInt($rootScope.selectedGroup),
+      method: 'GET',
+      dataType: 'json'
+    }).success(function (data, status, headers, config) {
+
+      // If data exists then display variables
+      if (data) {
+
+        $scope.group = data[0];
+
+      }
+
+    }).error(function (data){
+
+        // In case of an error display the message in console
+        console.log("error: " + data);
+
+    });
+  }
+  
+  $scope.isOwner = function(){
+	if($scope.group.owner == sessionStorage.getItem("email"))
+		return true;
+	else
+		return false;
+  }
+  
+  $scope.isOwner = function(){
+    if($scope.group){
+    	if($scope.group.owner == sessionStorage.getItem("email"))
+    		return true;
+    	else
+    		return false;
+    } else {
+      return false;
+    }
+	}
+  
+   $scope.isNotMember = function(){
+		if($scope.group){
+      if($scope.group.members.indexOf(sessionStorage.getItem("email")) < 0){
+  			return true;
+  		} else {
+  			return false;
+  		}
+    } else {
+      return false;
+    }
+	}
+
+  $scope.joinGroup = function(){
+    $http({
+      url: URL + '/request_entry/'+$scope.group.group_id,
+      method: 'POST',
+      dataType: 'json',
+      data: {"email": sessionStorage.getItem("email")},
+    }).success(function (data, status, headers, config) {
+
+      // If data exists then display variables
+      if (data) {
+
+        alert("Request sent. Wait for approval.");
+
+      }
+
+    }).error(function (data){
+
+        // In case of an error display the message in console
+        alert("Error: Unable to request your entrance. Please try again later.");
+
+    });
+  }
+
+
+  $scope.approveReq = function(){
+    $http({ // POST -- /group/:group_id/approve_request
+      url: URL + '/group/'+$scope.newgroup.group_id+"/approve_request",
+      method: 'POST',
+      dataType: 'json',
+      data: {"email": this.entry_req},
+    }).success(function (data, status, headers, config) {
+
+      // If data exists then display variables
+      if (data) {
+        $scope.newgroup.members.push(data.member);
+        $scope.newgroup.entry_reqs.splice($scope.newgroup.entry_reqs.indexOf(data.member),1);
+        alert("Request approved successfully.");
+
+      }
+
+    }).error(function (data){
+
+        // In case of an error display the message in console
+        alert("Error: Fail to update the request entry.");
+
+    });
+  }
+  
+  $scope.rejectReq = function(){
+    $http({ // POST -- /group/:group_id/reject_rquest
+      url: URL + '/group/'+$scope.newgroup.group_id+"/reject_rquest",
+      method: 'POST',
+      dataType: 'json',
+      data: {"email": this.entry_req},
+    }).success(function (data, status, headers, config) {
+
+      // If data exists then display variables
+      if (data) {
+        $scope.newgroup.entry_reqs.splice($scope.newgroup.entry_reqs.indexOf(this.entry_req),1);
+        alert("Request removed successfully.");
+
+      }
+
+    }).error(function (data){
+
+        // In case of an error display the message in console
+        alert("Error: Fail to update the request entries.");
+
+    });
+  }
+
+	$scope.getClass = function(path) {
+		if ($location.path().substr(0, path.length) == path) {
+		  return "active"
+		} else {
+		  return ""
+		}
+	}
+
+  $scope.initOwnGroups = function() {
+    $http({
+      url: URL + '/group/by_owner/'+sessionStorage.getItem("email"),
+      method: 'GET',
+      dataType: 'json',
+      data: '',
+    }).success(function (data, status, headers, config) {
+
+      // If data exists then display variables
+      if (data) {
+
+        $scope.groups = data;
+
+      }
+
+    }).error(function (data){
+
+        // In case of an error display the message in console
+        console.log("error: " + data);
+
+    });
+  }
+
+});
+
+
+
+
+
